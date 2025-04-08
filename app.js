785c7389098bc47febbc453c192795f3ac148e45
Shiv Patel <96531460+Pshiv2002@users.noreply.github.com>
2025-04-08 19:50:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-  origin: ["FRONTEND_URL", "DASHBOARD_URL"], // Allow multiple frontends
+  origin: ["https://hospital-management-system-sp.netlify.app", "https://admin-panel-sp.netlify.app"], // Allow multiple frontends
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
