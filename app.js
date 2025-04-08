ae4818cb7c51b01728d656f3e40bf0ade86c0f2e
Shiv Patel <96531460+Pshiv2002@users.noreply.github.com>
2025-04-08 19:43:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Allow multiple frontends
+  origin: ["FRONTEND_URL", "DASHBOARD_URL"], // Allow multiple frontends
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
