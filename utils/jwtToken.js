94b39372852fa108a1f78f85067e8eefc27e5905
Shiv Patel <96531460+Pshiv2002@users.noreply.github.com>
2025-04-13 17:27:27 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -13,7 +13,7 @@ export const generateToken = (user, message, statusCode, res) => {
       expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: true, // Change to true in production
-      sameSite: "Strict",
+      sameSite: "strict",
     })
     .json({
       success: true,
